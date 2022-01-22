aa661f6dce0894ab1c6f78795c734d5b8fa4f1e4
ezbueno <ezandrobueno@yahoo.com.br>
2022-01-22 18:10:15 -0300

    Adicionado as anotações do Swagger para documentação da API

--- src/main/java/com/buenoezandro/users/controller/AddressController.java
+++ src/main/java/com/buenoezandro/users/controller/AddressController.java
@@ -3,12 +3,15 @@ package com.buenoezandro.users.controller;
 import com.buenoezandro.users.model.Address;
 import com.buenoezandro.users.model.dto.AddressDTO;
 import com.buenoezandro.users.service.AddressService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Api(value = "Address")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/address")
@@ -16,11 +19,13 @@ public class AddressController {
     private final AddressService addressService;
 
     @GetMapping(path = "/{id}")
+    @ApiOperation(value = "Find address by id operation")
     public AddressDTO findById(@PathVariable(value = "id") Integer address_id) {
         return this.addressService.findById(address_id);
     }
 
     @PostMapping(path = "/create")
+    @ApiOperation(value = "Address creation operation")
     public ResponseEntity<AddressDTO> create(@RequestBody Address address) {
         var addressDTO = this.addressService.create(address);
         var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(addressDTO.getId()).toUri();
@@ -29,6 +34,7 @@ public class AddressController {
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @PutMapping(path = "/update/{id}")
+    @ApiOperation(value = "Address update operation")
     public void update(@PathVariable(value = "id") Integer id, @RequestBody Address address) {
         this.addressService.update(id, address);
     }
--- src/main/java/com/buenoezandro/users/controller/UserController.java
+++ src/main/java/com/buenoezandro/users/controller/UserController.java
@@ -3,6 +3,8 @@ package com.buenoezandro.users.controller;
 import com.buenoezandro.users.model.UserModel;
 import com.buenoezandro.users.model.dto.UserModelDTO;
 import com.buenoezandro.users.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(value = "USER API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/users")
@@ -19,16 +22,19 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
+    @ApiOperation(value = "List all registered users")
     public List<UserModelDTO> findAllUsers() {
         return this.userService.findAllUsers();
     }
 
     @GetMapping(path = "/{id}")
+    @ApiOperation(value = "Find user by id operation")
     public UserModelDTO findById(@PathVariable(value = "id") Integer id) {
         return this.userService.findById(id);
     }
 
     @PostMapping(path = "/create")
+    @ApiOperation(value = "User creation operation")
     public ResponseEntity<UserModelDTO> create(
             @RequestParam(value = "address", defaultValue = "0") Integer address_id,
             @Valid @RequestBody UserModel userModel
@@ -40,12 +46,14 @@ public class UserController {
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @PutMapping(path = "/update/{id}")
+    @ApiOperation(value = "User update operation")
     public void update(@PathVariable(value = "id") Integer id, @Valid @RequestBody UserModel userModel) {
         this.userService.update(id, userModel);
     }
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @DeleteMapping(path = "/delete/{id}")
+    @ApiOperation(value = "Delete user by id operation")
     public void delete(@PathVariable(value = "id") Integer id) {
         this.userService.delete(id);
     }
