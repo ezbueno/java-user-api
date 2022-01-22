cce7dfb852c28dc4710faa0bd6e70b3f3d88d0fb
ezbueno <ezandrobueno@yahoo.com.br>
2022-01-22 18:09:42 -0300

    Classe de configuração do Swagger

--- src/main/java/com/buenoezandro/users/config/SwaggerConfig.java
+++ src/main/java/com/buenoezandro/users/config/SwaggerConfig.java
@@ -1,2 +1,43 @@
-package com.buenoezandro.users.config;public class SwaggerConfig {
+package com.buenoezandro.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String BASE_PACKAGE = "com.buenoezandro.users";
+    private static final String API_TITLE = "USER";
+    private static final String API_DESCRIPTION = "USER API";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Ezandro Bueno";
+    private static final String CONTACT_GITHUB = "https://github.com/ezbueno";
+    private static final String CONTACT_EMAIL = "ezandro.developer@gmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
 }
